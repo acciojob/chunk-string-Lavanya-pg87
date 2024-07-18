4b1370e02268e09d73f66a8ca5168a7be89ad164
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-18 19:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
   // your code here
+	let arr = [];
+	let i=0;
+	while(i< str.length){
+		let x = str.slice(i,i+size);
+		arr.push(x);
+		i = i + size;
+	}
+return arr;
 }
 
 // Do not change the code below
